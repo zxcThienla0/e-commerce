d4c8214a1c72ce515ba590ad9952d5ba87484ae4
Burkalev Semyon <gladodinochka@mail.ru>
2025-09-14 11:19:38 +0300

    fix 4 homepage

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+   base: '/e-commerce/',
 })
